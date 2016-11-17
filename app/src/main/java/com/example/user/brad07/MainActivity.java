1ad0f2fd56e857ec3c5e935ad753a3108a47c46b
Brad Chao <brad@brad.tw>
2016-11-17 15:33:50 +0800

    v2

--- app/src/main/java/com/example/user/brad07/MainActivity.java
+++ app/src/main/java/com/example/user/brad07/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.user.brad07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button left, right;
+    private TextView clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        left = (Button)findViewById(R.id.btnLeft);
+        right = (Button)findViewById(R.id.btnRight);
+        clock = (TextView)findViewById(R.id.timer);
+
     }
+    public void doLeft(View v){
+
+    }
+    public void doRight(View v){
+
+    }
+
+
+
 }
